668e079f76661a81e5e7600c468c3ba7445b7d8a
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2025-02-14 11:33:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
+  base: "/February-Declaration/", // 👈 Usa el nombre de tu repositorio en GitHub
 })
