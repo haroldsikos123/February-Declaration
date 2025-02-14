d54c6a701925a5b6b684e4c6bb58042b0efe7a21
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2025-02-14 12:58:41 -0500

    holaa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "",  // ❌ Antes: "/Puedo-ser-tu-san-valentin-nonita/"
+  base: "./", // Cambia esto para evitar errores de ruta en GitHub Pages
 })
