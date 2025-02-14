0dc98d49a30ab69e4c6577182e7a823f5dbc4fdf
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2025-02-14 13:07:30 -0500

    nuevo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Cambia esto para evitar errores de ruta en GitHub Pages
-})
+  base: "./", // 👈 Usa el nombre exacto de tu repo
+});
